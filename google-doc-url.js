b382fdeacbeed03e49d7181be191311d5adca494
EmiliaSUczelnia <117667175+EmiliaSUczelnia@users.noreply.github.com>
2022-11-07 20:32:49 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Tg3bTLNuSb2qAuXhqLC2ts3y5mtcQs8yj0I4Ib2n0dk/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1NqXY0jjTNbo1jXlF1nANkidf-qDug3Z8FDO6TVellD0/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
